02cd5525db2785a10e74a3fcb48f5d11e900a84e
knRiya <65356642+xknRiya@users.noreply.github.com>
2023-05-23 11:44:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+/* const fs = require('fs');
+fs.readFile('quesitons.txt', 'utf8', function(err, data){
+    if (err){
+        console.error(err);
+        return;
+    }
+    const lines = data.split('\n');
+    lines.forEach(function(line){
+        console.log(line)
+    })
+})*/
